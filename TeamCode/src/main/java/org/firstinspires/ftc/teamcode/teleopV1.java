32e2f182e5f8c234a212e728c4de8d096863df46
sohnNa <nate39utopia@gmail.com>
2025-09-24 15:28:37 -0700

    Ready for first test^2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopV1.java
@@ -54,7 +54,7 @@ public class teleopV1 extends LinearOpMode {
         // Adjust the orientation parameters to match your robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
-                RevHubOrientationOnRobot.UsbFacingDirection.LEFT));
+                RevHubOrientationOnRobot.UsbFacingDirection.DOWN));
         // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
         imu.initialize(parameters);
         
