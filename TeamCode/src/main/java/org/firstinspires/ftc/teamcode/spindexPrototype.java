be9cf647a356174139c7a383799f78828ecb0d64
sohnNa <nate39utopia@gmail.com>
2025-12-05 17:00:21 -0800

    Spindexer code debugging, found lose spindexer is causing problems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/spindexPrototype.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/spindexPrototype.java
@@ -1,20 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
-import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 
 @TeleOp(name = "spindexPrototype")
@@ -28,11 +18,27 @@ public class spindexPrototype extends LinearOpMode {
     //dashboard = FtcDashboard.getInstance();
     @Override
     public void runOpMode() throws InterruptedException {
-        boolean limitCondition1 = false;
+        boolean limitCondition1 = true;
 
-        boolean limitCondition2 = false;
+        boolean launching = false;
+
+        boolean forceRun = true;
+
+        boolean toLaunch = false;
+
+        boolean running = false;
+
+        boolean runningTwo = false;
+
+        boolean wantToLaunch = false;
+
+        int sleepCounter = 0;
+        boolean isSleeping = false;
+
 
         spindexServo = hardwareMap.get(CRServo.class, "spindexServo");
+
+        Servo spatulaServo = hardwareMap.servo.get("spatulaServo");
 
         limitOne = hardwareMap.get(TouchSensor.class, "limitOne");
         limitTwo = hardwareMap.get(TouchSensor.class, "limitTwo");
@@ -45,39 +51,78 @@ public class spindexPrototype extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            /*
-            if (gamepad1.dpad_right) {
-                spindexServo.setPower(0.75);
-            } else if (gamepad1.dpad_left) {
-                spindexServo.setPower(-0.75);
-            } else {
-                spindexServo.setPower(0);
-            }
-            */
 
 
 
-            if (gamepad1.dpad_left && !limitCondition1) {
-                spindexServo.setPower(0.75);
-            } else if (gamepad1.dpad_right && !limitCondition1) {
-                spindexServo.setPower(-0.75);
-            } else {
-                spindexServo.setPower(0);
+
+            if (gamepad1.dpadRightWasPressed()) {
+                forceRun = true;
             }
 
-            if (limitOne.isPressed()) {
+            if (launching || limitCondition1) spindexServo.setPower(0.1);
+            //else if () spindexServo.setPower(0.75);
+            else spindexServo.setPower(0);
+
+            // if the limit switch is not pressed or an input is forcing it to run, then spin the spindexer
+            if (!wantToLaunch && (!limitOne.isPressed() || forceRun)) {
                 limitCondition1 = true;
-            } else {
-                limitCondition1 = false;
+                // potential issue in it wanting to go but launching preventing that, but wait to go off limit switch should fix it
+                running = true;
+            }
+            else limitCondition1 = false;
+
+            // waiting until outside the limit switch, then letting it stop when it hits a switch again
+            if (running && !limitOne.isPressed()) {
+                running = false;
+                forceRun = false;
+            }
+
+            // same thing but for the launching position
+            if (wantToLaunch && !limitTwo.isPressed()) {
+                launching = true;
+                // runningTwo = true;
+            }
+            else launching = false;
+
+//            if (runningTwo && !limitTwo.isPressed()) {
+//                runningTwo = false;
+//                toLaunch = false;
+//            }
+
+            if (gamepad1.a) {
+                wantToLaunch = true;
+            } else if (gamepad1.b) {
+                wantToLaunch = false;
             }
 
 
+            if (gamepad2.left_bumper) {
+                spatulaServo.setPosition(-1);
+            } else if (gamepad2.right_bumper && !limitCondition1 && !launching) {
+                spatulaServo.setPosition(1);
+            }
 
-            telemetry.addData("Servo power", spindexServo.getPower());
+            if (gamepad2.back && !limitCondition1 && !launching) {
+                spatulaServo.setPosition(1);
+                isSleeping = true;
+                sleepCounter = 0;
+            }
+            if (isSleeping && sleepCounter >= 30) {
+                isSleeping = false;
+                spatulaServo.setPosition(-1);
 
+            }
+            sleepCounter ++;
 
+            telemetry.addData("Servo power: ", spindexServo.getPower());
+            telemetry.addData("Magnetic Switch 1 is letting servo run: ", limitCondition1);
+            telemetry.addData("Not in launching position: ", launching);
+            // telemetry.addData(": ", toLaunch);
+            telemetry.addData("Going to next limit: ", forceRun);
+
+            // next line of telemetry needed in drive code:
+            telemetry.addData("In launching mode: ", wantToLaunch);
             telemetry.update();
         }
     }
 }
-
