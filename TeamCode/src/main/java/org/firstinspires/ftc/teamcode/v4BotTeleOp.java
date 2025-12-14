989c265306686590a77b5cc3535f088305ba7dd5
sohnNa <nate39utopia@gmail.com>
2025-12-13 16:38:45 -0800

    Does not have April Tags

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/v4BotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/v4BotTeleOp.java
@@ -14,17 +14,37 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import org.firstinspires.ftc.teamcode.Mechanisms.AprilTagWebcam;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 @TeleOp(name = "v4BotTeleOp")
+@Disabled
 public class v4BotTeleOp extends LinearOpMode {
+
+    AprilTagWebcam aprilTagWebcam = new AprilTagWebcam();
     TouchSensor limitOne;
 
     TouchSensor limitTwo;
 
+    double red = 0.0;
+    double green = 0.0;
+    double blue = 0.0;
+    String artifactStatus = "";
+    int counter = 0;
+    String artifactOne;
+
+    String artifactTwo;
+
+    String artifactThree;
 
     double speed;
 
@@ -34,24 +54,23 @@ public class v4BotTeleOp extends LinearOpMode {
 
     int next_pos = 0;
 
+    boolean launching = false;
+
+    boolean spatulaInWay = false;
+
+    boolean toFirstPos = true;
+
+    int SpindexPos = 0;
+
+
+
     @Override
     public void runOpMode() throws InterruptedException {
-        boolean launching = false;
 
-        boolean spatulaInWay = false;
-
-        int sleepCounter = 0;
-
-        boolean isSleeping = false;
-
-        boolean toFirstPos = true;
-
-        int SpindexPos = 0;
-
-        DcMotorEx frontLeft = hardwareMap.get(DcMotorEx.class,"frontLeft");
-        DcMotorEx backLeft = hardwareMap.get(DcMotorEx.class,"backLeft");
-        DcMotorEx frontRight = hardwareMap.get(DcMotorEx.class,"frontRight");
-        DcMotorEx backRight = hardwareMap.get(DcMotorEx.class,"backRight");
+        DcMotorEx frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        DcMotorEx backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+        DcMotorEx frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        DcMotorEx backRight = hardwareMap.get(DcMotorEx.class, "backRight");
 
         DcMotorEx intakeMotor = hardwareMap.get(DcMotorEx.class, "intakeMotor");
         DcMotorEx turretMotor = hardwareMap.get(DcMotorEx.class, "turretMotor");
@@ -69,11 +88,10 @@ public class v4BotTeleOp extends LinearOpMode {
         DcMotorEx flywheel = hardwareMap.get(DcMotorEx.class, "flywheel");
 
 
-
         // Reversing the motors.
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        aprilTagWebcam.init(hardwareMap, telemetry);
 
         // Retrieve the IMU from the hardware map
         IMU imu = hardwareMap.get(IMU.class, "imu");
@@ -133,8 +151,7 @@ public class v4BotTeleOp extends LinearOpMode {
 
             if (rx > 1) {
                 rx = 1;
-            }
-            else if (rx < -1) {
+            } else if (rx < -1) {
                 rx = -1;
             }
 
@@ -147,7 +164,7 @@ public class v4BotTeleOp extends LinearOpMode {
             }
 
             if (gamepad1.x) {
-                flywheel.setVelocity(2800);
+                flywheel.setVelocity(2000);
             } else if (gamepad1.b) {
                 flywheel.setVelocity(-1600);
             } else if (gamepad1.y) {
@@ -169,24 +186,21 @@ public class v4BotTeleOp extends LinearOpMode {
             }
 
 
+            if (gamepad1.leftStickButtonWasPressed()) {
+                toFirstPos = true;
+            }
 
 
             //Driver Controls. Speed control
-            if (gamepad2.right_bumper) {
+            if (gamepad2.left_bumper) {
                 speed = 0.5f;
             } else {
                 speed = 1.0f;
             }
 
-            if (gamepad2.left_bumper) {
+            if (gamepad2.right_bumper) {
                 imu.resetYaw();
             }
-
-
-
-
-
-
 
 
             // Rotate the movement direction counter to the bot's rotation
@@ -205,15 +219,13 @@ public class v4BotTeleOp extends LinearOpMode {
             double backRightPower = (rotY + rotX - rx) / denominator;
 
 
-
-            if (gamepad1.dpadRightWasPressed() && !toFirstPos  && !spatulaInWay) {
+            if (gamepad1.dpadRightWasPressed() && !toFirstPos && !spatulaInWay) {
                 // rotating between intake positions
                 SpindexPos += 96;
                 spindexMotor.setTargetPosition(SpindexPos);
                 spindexMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 spindexMotor.setVelocity(100);
-            }
-            else if (gamepad1.dpadLeftWasPressed() && !toFirstPos  && !spatulaInWay) {
+            } else if (gamepad1.dpadLeftWasPressed() && !toFirstPos && !spatulaInWay) {
                 // rotating between intake positions
                 SpindexPos -= 96;
                 spindexMotor.setTargetPosition(SpindexPos);
@@ -239,6 +251,35 @@ public class v4BotTeleOp extends LinearOpMode {
                 spatulaServo.setPosition(1);
                 spatulaInWay = true;
             }
+
+
+            green = colorSensor.green();
+            red = colorSensor.red();
+            blue = colorSensor.blue();
+
+            if ((green < 100) && (red < 100) && (blue < 100)) {
+                artifactStatus = "N/A or Hole";
+            } else if (green > blue) {
+                artifactStatus = "Green Artifact";
+            } else if (blue > green) {
+                artifactStatus = "Purple Artifact";
+            }
+
+            if (gamepad1.dpadRightWasPressed()) {
+                if (counter == 0) {
+                    artifactOne = artifactStatus;
+                    counter += 1;
+                } else if (counter == 1) {
+                    artifactTwo = artifactStatus;
+                    counter += 1;
+                } else if (counter == 2) {
+                    artifactThree = artifactStatus;
+                    counter = 0;
+                }
+            }
+
+
+
             /*
             if (gamepad2.back && launching && spindexMotor.getVelocity() < 300) {
                 spatulaServo.setPosition(1);
@@ -254,24 +295,31 @@ public class v4BotTeleOp extends LinearOpMode {
             }
 
              */
-            sleepCounter ++;
+
+            aprilTagWebcam.update();
+            AprilTagDetection id20 = aprilTagWebcam.getTagsBySpecificId(20);
+            aprilTagWebcam.displayDetectionTelemetry(id20);
+
+
 
             //Spindexer Telemetry
-            telemetry.addData("Spindexer speed", spindexMotor.getVelocity());
+            //telemetry.addData("Spindexer speed", spindexMotor.getVelocity());
             telemetry.addData("Magnetic Switch is pressed", limitOne.isPressed());
             telemetry.addData("SpatulaInWay", spatulaInWay);
-            telemetry.addData("Spindexer Position", spindexMotor.getCurrentPosition());
-            telemetry.addData("Spindexer Target Position", spindexMotor.getTargetPosition());
+            //telemetry.addData("Spindexer Position", spindexMotor.getCurrentPosition());
+            //telemetry.addData("Spindexer Target Position", spindexMotor.getTargetPosition());
             telemetry.addData("In launching position", launching);
             //Intake & Output motor Telemetry
-            telemetry.addData("Turret Velocity", turretMotor.getVelocity());
-            telemetry.addData("Intake Velocity", intakeMotor.getVelocity());
+            //telemetry.addData("Turret Velocity", turretMotor.getVelocity());
+            //telemetry.addData("Intake Velocity", intakeMotor.getVelocity());
             telemetry.addData("Flywheel", flywheel.getVelocity());
             //Color Sensor Telemetry
+            telemetry.addData("Artifact Status", artifactStatus);
+            /*
             telemetry.addData("Red", colorSensor.red());
             telemetry.addData("Green", colorSensor.green());
             telemetry.addData("Blue", colorSensor.blue());
-
+            */
             // next line of telemetry needed in drive code:
 
             telemetry.update();
@@ -284,6 +332,11 @@ public class v4BotTeleOp extends LinearOpMode {
 
             prevTime = runtime.milliseconds();
 
+
         }
     }
+
+
 }
+
+
